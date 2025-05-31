7651ffb662048f0a0d1df6c7bcd3e994e64fc668
Varda Kamat <118811858+varda-kamat@users.noreply.github.com>
2025-05-31 22:48:05 +0530

    Update Predict.jsx

--- app/dashboard/_components/Predict.jsx
+++ app/dashboard/_components/Predict.jsx
@@ -1,5 +1,5 @@
 "use client";
-
+//Hii
 import React, { useState, useEffect } from "react";
 import { Card, CardHeader, CardTitle, CardContent } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
